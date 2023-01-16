0276b7da70df058603adb5f04b37df859cd99374
Tomasz Cebula <tcebula@pm.me>
2023-01-16 20:56:16 +0100

    Added flag to output a premade systemd unit file which will run the program if installed to /usr/bin

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use sqlx::sqlite::{Sqlite, SqliteConnectOptions, SqlitePoolOptions};
 use sqlx::{migrate::Migrator, query, Pool};
 
 #[derive(Parser)]
+#[command(version, about)]
 struct Cli {
     /// Access token for your bot. Required to run.
     #[arg(short, long, required(true))]
@@ -16,6 +17,9 @@ struct Cli {
     /// Optional guild id to connect to. Useful for testing, speeds up registering of commands.
     #[arg(short, long)]
     guild: Option<u64>,
+    /// Print a premade systemd unit with your options.
+    #[arg(long = "make-systemd-unit")]
+    unit: bool,
 }
 
 struct Data {
@@ -137,6 +141,10 @@ async fn random(
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
+    if cli.unit {
+        println!("{}", systemd_unit(&cli.token, &cli.database, cli.guild));
+        return;
+    }
     let data = Data::from(cli.guild, cli.database).await;
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
@@ -152,11 +160,30 @@ async fn main() {
                     poise::builtins::register_in_guild(ctx, &framework.options().commands, guild)
                         .await
                         .expect("Invalid Guild Id");
-                    println!("Registered guild commands!");
+                    println!("Registering guild commands!");
                 };
+                println!("Registering global commands");
                 Ok(data)
             })
         });
 
     framework.run().await.unwrap();
+}
+
+fn systemd_unit(tok: &str, db: &Option<String>, guild: Option<u64>) -> String {
+    let mut unit = format!(
+        r"[Unit]
+Description=Discord quote bot
+[Service]
+ExecStart=/usr/bin/discord_quote_bot --token {}",
+        tok
+    );
+    if let Some(database) = db {
+        unit.push_str(&format!("--database {}", database));
+    }
+    if let Some(guild) = guild {
+        unit.push_str(&format!("--guild {}", guild.to_string()));
+    }
+    unit.push_str("\n[Install]\nWantedBy=multi-user.target");
+    unit
 }
